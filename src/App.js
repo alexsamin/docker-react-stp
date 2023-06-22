a6a3382b6592561df2385b8a9bb933f4ad8c1f06
alexsamin <134327075+alexsamin@users.noreply.github.com>
2023-06-22 20:37:59 +0530

    updated Dockerfile

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          React App running on a Docker Container !!!
+          with Docker Compose
+        </p>      
         <a
           className="App-link"
           href="https://reactjs.org"
